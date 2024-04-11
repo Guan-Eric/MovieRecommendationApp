e090bcf0687892d81faceb48ff400440ab245a7c
guan-eric <erictheguan@gmail.com>
2024-04-11 14:40:52 -0400

    Setup backend

---
+++ backend/movieRecommendor/src/main/java/com/example/movieRecommendor/MovieRecommendorApplication.java
@@ -0,0 +1,13 @@
+package com.example.movieRecommendor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRecommendorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieRecommendorApplication.class, args);
+	}
+
+}
---
+++ backend/movieRecommendor/src/test/java/com/example/movieRecommendor/MovieRecommendorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.movieRecommendor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRecommendorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
